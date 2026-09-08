b46e694bf476a2c52e28589d62d693d8a4ad0e87
Shahzad <sh.heydari55@gmail.com>
2026-09-08 11:53:04 +0330

    Restore _worker.js with English admin panel translation

--- _worker.js
+++ _worker.js
@@ -1,1 +1,3 @@
-PLACEHOLDER
+const Version = '2026-09-04 16:24:13';
+// TEMPORARY STUB - restore in progress
+export default { async fetch() { return new Response('Worker restore in progress. Please wait.', { status: 503 }); } };
